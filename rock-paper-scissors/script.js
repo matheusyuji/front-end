149dda8cc89f1092c6739667dc00ff6663e38af4
Matheus Yuji Morishita <matheusmorishita@hotmail.com>
2025-02-02 18:31:38 -0300

    [add]: created the getComputerChoice function

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,18 @@
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getComputerChoice() {
+  let choice = getRandomNumber(1, 3);
+  switch (choice) {
+    case 1:
+      choice = "ROCK";
+      return choice;
+    case 2:
+      choice = "PAPER";
+      return choice;
+    case 3:
+      choice = "SCISSORS";
+      return choice;
+  }
+}
