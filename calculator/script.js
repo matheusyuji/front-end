c4e6e92920964d97437b7501146b7c536d7a351e
Matheus Yuji Morishita <matheusmorishita@hotmail.com>
2025-02-04 16:35:39 -0300

    [add]: created functions for all of the basic math operators

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+function add (a, b) {
+  return a + b;
+}
+
+function sub (a, b) {
+  return a - b;
+}
+
+function mult (a, b) {
+  return a * b;
+}
+
+function div (a, b) {
+  return a / b;
+}
+
+let a = 2
+let b = 2
+
+console.log(add(a, b));
+console.log(sub(a, b));
+console.log(mult(a, b));
+console.log(div(a, b));
