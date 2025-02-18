f9b852a8f26ec8857bdd9f4c8000a11c6be2e716
Matheus Yuji Morishita <matheusmorishita@hotmail.com>
2025-02-18 13:36:11 -0300

    [feat]: possible to move between modules using the buttons

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -3,6 +3,46 @@ import { home } from "./modules/home.js";
 import { menu } from "./modules/menu.js";
 import { contact } from "./modules/contact.js";
 
-//home.render();
-//menu.render();
-//contact.render();
+const webSite = {
+  pages: {
+    home: home,
+    menu: menu, 
+    contact: contact
+  },
+
+  currentPage: "home",
+
+  init: function() {
+    this.cacheDom();
+    this.bindEvents();
+    this.render();
+ },
+
+  cacheDom: function() {
+    this.buttons = document.querySelectorAll(".btn-header");
+    this.content = document.querySelector("#content");
+  },
+
+  bindEvents: function() {
+    this.buttons.forEach(button => {
+       button.addEventListener("click", (event) => this.handlePageChange(event));
+    });
+  },
+
+  handlePageChange: function(event) {
+    const page = event.target.dataset.page;
+    if (this.currentPage !== page) {
+       this.pages[page].render();
+       this.currentPage = page;
+    }
+  },
+
+  render: function() {
+    this.pages[this.currentPage].render();
+  }
+};
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  webSite.init();
+});
